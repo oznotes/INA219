0c08d984d1d189b522c05d6f9e05cb5e64542fe2
Sam <35919701+SamSkjord@users.noreply.github.com>
2023-07-18 19:41:26 +0100

    I2C bus selection fix
    Initial comit has hardcoded values for i2c bus despite it being an argument in I2C_START

--- INA219_driver/INA219.h
+++ INA219_driver/INA219.h
@@ -8,9 +8,8 @@
 
 class INA219 {
 public:
-    INA219(uint8_t i2c_addr = INA219_I2C_ADDR);
-
-    void I2C_START(i2c_inst_t *i2c_instance, uint32_t sda_pin, uint32_t scl_pin, uint32_t speed_khz);
+    INA219(i2c_inst_t *i2c_instance, uint8_t i2c_addr);
+    void I2C_START(uint32_t sda_pin, uint32_t scl_pin, uint32_t speed_khz);
     void init();
     void calibrate(float shunt_resistor_value, float max_expected_amps);
     float read_voltage();
@@ -22,7 +21,7 @@ private:
     uint16_t read_register(uint8_t reg);
     void write_register(uint8_t reg, uint16_t value);
 
-    static constexpr uint8_t INA219_I2C_ADDR = 0x40;
+    //static constexpr uint8_t INA219_I2C_ADDR = 0x40;
     static constexpr uint8_t INA219_REG_CONFIG = 0x00;
     static constexpr uint8_t INA219_REG_SHUNTVOLTAGE = 0x01;
     static constexpr uint8_t INA219_REG_BUSVOLTAGE = 0x02;
@@ -31,6 +30,7 @@ private:
     static constexpr uint8_t INA219_REG_CALIBRATION = 0x05;
 
     uint8_t _i2c_addr;
+    i2c_inst_t *_i2c_instance;
     float _current_LSB;
 };
 
